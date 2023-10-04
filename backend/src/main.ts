e39d68a2b3c776fdcb9185d8588451127f440171
stanislav.trocuk <stanislav.trotsuk@fmobile.by>
2023-10-04 09:16:06 +0300

    changed config settings for connection postgres database

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,15 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  try{
-    const PORT = process.env.PORT || 5000
+  try {
+    const PORT = process.env.PORT || 5000;
     const app = await NestFactory.create(AppModule);
-    await app.listen(PORT,()=>{
-      return console.log('backend started on '+PORT + ' port')
+    await app.listen(PORT, () => {
+      return console.log('backend started on ' + PORT + ' port');
     });
-  }catch(e){
-    console.log(e)
+  } catch (e) {
+    console.log(e);
   }
-
 }
 bootstrap();
