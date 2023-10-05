4d57c84889034ef1bc66b90647157f93430a5f00
stanislav.trocuk <stanislav.trotsuk@fmobile.by>
2023-10-05 10:20:40 +0300

    added TrackModule & TrackEntity

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { configService } from './config/config.service';
+import { TrackModule } from './track/track.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot(configService.getTypeOrmConfig())
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    TrackModule
   ],
   controllers: [],
   providers: [],
---
+++ backend/src/track/entities/track.entity.ts
@@ -0,0 +1,25 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class TrackEntity{
+  @PrimaryGeneratedColumn()
+  id:number;
+
+  @Column()
+  name: string
+
+  @Column()
+  musician: string
+
+  @Column()
+  description: string
+
+  @Column({name:'picture', type:'bytea',nullable:true })
+  picture: Buffer;
+
+  @Column({ type: 'bytea',name:'audio',nullable:true })
+  audio: Uint8Array;
+
+  @Column()
+  listens: number
+}
